9d996b5b2e8327de47c6478253c25f5a09974b32
Patchnote-v2 <11915225+Patchnote-v2@users.noreply.github.com>
2025-03-16 13:07:27 -0700

    Added hotkey to search Treesitter using Telescope

--- lua/patchnote/lazy/telescope.lua
+++ lua/patchnote/lazy/telescope.lua
@@ -59,6 +59,7 @@ return
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
         vim.keymap.set('n', '<leader>sb', builtin.current_buffer_fuzzy_find, { desc = '[S]earch current [B]uffer' })
+        vim.keymap.set('n', '<leader>st', builtin.treesitter, { desc = '[S]earch [T]ree' })
         vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
         -- Slightly advanced example of overriding default behavior and theme
